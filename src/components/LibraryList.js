824c9d878821213987c2e5df57618811b21cb28f
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-18 15:55:58 -0400

    adds libraryList

---
+++ src/components/LibraryList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class LibraryList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return;
+    }
+}
+
+export default LibraryList;
