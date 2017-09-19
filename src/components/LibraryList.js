972681f3d2bc8464f29a1a8dbf37e3f53e988666
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-19 08:26:01 -0400

    adds ListView

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
@@ -6,9 +7,21 @@ class LibraryList extends Component {
         super(props);
     }
 
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
     render() {
-        console.log(this.props);
-        return;
+        return (
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        );
     }
 }
 
