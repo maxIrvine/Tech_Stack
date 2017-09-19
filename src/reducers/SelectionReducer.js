661ffacfc34c902200b6908eb6cbb585f97dfed9
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-19 09:12:03 -0400

    adds switch statement

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,4 +1,8 @@
 export default (state, action) => {
-    console.log(action);
-    return null;
+    switch (action.type) {
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+    }
 };
