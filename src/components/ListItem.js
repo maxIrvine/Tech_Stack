7a7899c1b61a00cd8ff6b22fde0ab10e4e44f7dc
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-19 09:55:52 -0400

    adds style of flex 1

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -15,7 +15,9 @@ class ListItem extends Component {
 
         if (expanded) {  
             return (
-                <Text>{library.description}</Text>
+                <CardSection>
+                    <Text style={{ flex: 1 }}>{library.description}</Text>
+                </CardSection>
             );
         }
     }
