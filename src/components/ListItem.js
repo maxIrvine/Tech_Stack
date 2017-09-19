8909a0fb76befc865597b70e6e54907acb1460e4
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-19 08:32:02 -0400

    adds ListItem

---
+++ src/components/ListItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ListItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+    }
+}
+
+export default ListItem;
