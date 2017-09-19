d66eb41b7e3ade69b0a9f3077b47be1a4182c822
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-09-19 08:38:27 -0400

    adds flex 1

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
 
 
 class ListItem extends Component {
@@ -7,7 +9,20 @@ class ListItem extends Component {
     }
 
     render() {
+        const { titleStyle } = styles;
 
+        return (
+            <CardSection>
+                <Text style={titleStyle}>{this.props.library.title}</Text>
+            </CardSection>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
     }
 }
 
